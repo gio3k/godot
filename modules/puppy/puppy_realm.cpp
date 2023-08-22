ecbd5759bf37f1a3a68f5a57047b9ccd40a24af8
Gio <lotuspar0@gmail.com>
2023-08-23 03:53:10 +0800

    puppy: `PuppyRealm` _poll impl, more progress

---
+++ modules/puppy/puppy_realm.cpp
@@ -0,0 +1,40 @@
+﻿#include "puppy_realm.h"
+
+Error PuppyRealm::_poll() {
+	Error err;
+
+	if (!_peer.is_valid()) {
+		// Invalid peer - set connection status to disconnected and return
+		_connection_status = MultiplayerPeer::CONNECTION_DISCONNECTED;
+		return OK;
+	}
+
+	// Update connection status
+	_connection_status = _peer->get_connection_status();
+	if (_connection_status != MultiplayerPeer::CONNECTION_CONNECTED)
+		return OK; // We aren't connected - just return OK for now
+
+	// Update the realm
+	if (err = _update(); err != OK)
+		return err;
+
+	// Get all packets from peer, call _handle for each
+	while (_peer->get_available_packet_count() != 0) {
+		const int sender = _peer->get_packet_peer();
+		const int channel = _peer->get_packet_channel();
+		const MultiplayerPeer::TransferMode mode = _peer->get_packet_mode();
+
+		const uint8_t *buffer;
+		int len;
+
+		// Attempt to get the packet
+		err = _peer->get_packet(&buffer, len);
+		ERR_FAIL_COND_V_MSG(err != OK, err, "Couldn't get packet while polling realm");
+
+		// Call _handle
+		_handle(buffer, len, sender, channel, mode);
+	}
+
+	// We're done here!
+	return OK;
+}
--- modules/puppy/puppy_realm.h
+++ modules/puppy/puppy_realm.h
@@ -8,23 +8,55 @@ class PuppyMultiplayer;
 class PuppyRealm : public RefCounted {
 	GDCLASS(PuppyRealm, RefCounted)
 
+	friend PuppyMultiplayer;
+
+	/**
+	 * \brief Poll the peer and call update() if possible
+	 */
+	Error _poll();
+
+	MultiplayerPeer::ConnectionStatus _connection_status = MultiplayerPeer::CONNECTION_DISCONNECTED;
+
 protected:
 	/**
-	 * \brief This is our "peer" - we use this to talk to other "peers" over the internet.
-	 * Keep in mind this can be a server or client, or anything really. It's just a way to talk
-	 * to other peers.
+	 * \brief This is our "peer" - we use this to talk to other peers.
+	 * Keep in mind - this can be for a server, a client, or anything really. It's just used
+	 * to communicate to other peers.
 	 */
-	Ref<MultiplayerPeer> peer;
+	Ref<MultiplayerPeer> _peer;
+
+	/**
+	 * \brief Puppy multiplayer core controller
+	 */
+	PuppyMultiplayer *_multiplayer;
+
+	/**
+	 * \brief Called every time the peer is polled
+	 * \return Error
+	 */
+	virtual Error _update() = 0;
+
+	/**
+	 * \brief Called for every packet found after polling
+	 * \param buffer Pointer to packet data
+	 * \param len Packet length
+	 * \param sender Packet sender
+	 * \param channel Packet channel
+	 * \param mode Packet transfer mode
+	 */
+	virtual void _handle(const uint8_t *buffer, int len, int sender, int channel, MultiplayerPeer::TransferMode mode) = 0;
 
 public:
-	explicit PuppyRealm(PuppyMultiplayer *core);
+	explicit PuppyRealm(PuppyMultiplayer *p_multiplayer) {
+		_multiplayer = p_multiplayer;
+	}
 
 	void set_multiplayer_peer(const Ref<MultiplayerPeer> &p_peer) {
-		peer = p_peer;
+		_peer = p_peer;
 	}
 
 	Ref<MultiplayerPeer> get_multiplayer_peer() {
-		return peer;
+		return _peer;
 	}
 };
 
