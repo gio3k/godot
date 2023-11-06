fc1cf8f3fccaa49f9f67574d898e6b57f7d4c7e8
Gio <lotuspar0@gmail.com>
2023-11-07 00:03:02 +0800

    libnx: initial compile cfg work

--- platform/libnx/detect.py
+++ platform/libnx/detect.py
@@ -7,6 +7,10 @@ def get_name():
 
 
 def can_build():
+    if "DEVKITPRO" not in os.environ:
+        print("Please set DEVKITPRO in your environment. export DEVKITPRO=<path to>/devkitpro")
+        return False
+
     return True
 
 
@@ -19,4 +23,23 @@ def get_flags():
 
 
 def configure(env):
-    print(env)
+    # devkitpro setup
+    devkitpro = os.environ.get("DEVKITPRO")
+    devkitpro_bin = os.path.join(devkitpro, "devkitA64/bin")
+
+    # Compiler / toolchain setup
+    env["CC"] = os.path.join(devkitpro_bin, "aarch64-none-elf-gcc")
+    env["CXX"] = os.path.join(devkitpro_bin, "aarch64-none-elf-g++")
+    env["LD"] = os.path.join(devkitpro_bin, "aarch64-none-elf-ld")
+    env["AR"] = os.path.join(devkitpro_bin, "aarch64-none-elf-ar")
+    env["RANLIB"] = os.path.join(devkitpro_bin, "aarch64-none-elf-ranlib")
+
+    # Architecture setup
+    arch_flags = ["-march=armv8-a+crc+crypto", "-mtune=cortex-a57", "-mtp=soft", "-fPIE"]
+    env.Append(ASFLAGS=arch_flags)
+    env.Append(CCFLAGS=arch_flags)
+    env.Append(LINKFLAGS=arch_flags)
+
+    # Platform setup
+    env.Prepend(CPPPATH=["#platform/libnx"])
+    env.Append(CPPDEFINES=["HORIZON_ENABLED"])
